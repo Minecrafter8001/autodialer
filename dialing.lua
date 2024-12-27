ea849e1c4c1645c12b6921065ba0609ef613809c
minecrafter8001 <133237713+Minecrafter8001@users.noreply.github.com>
2024-12-27 15:40:05 +0000

    modified:   dialing.lua

--- dialing.lua
+++ dialing.lua
@@ -1,63 +1,24 @@
--- dialing.lua
 local addressManager = require("autoDialer.addressManager")
 local config = require("autoDialer.config")
 
-local function dialermode(address)
-    while true do
-    print("Choose dialing mode: (1) Fast, (2) Slow")
-    local mode = read()
-    if mode == "1" then
-        fastDialStargate(address)
-    elseif mode == "2" then
-        slowDialStargate(address)
-    else
-        print("Invalid option, please try again.")
+-- Fast dialing for Stargate
+local function fastDialStargate(address)
+    local sg = peripheral.find("advanced_crystal_interface")
+    print("Dialing Stargate...")
+
+    for _, symbol in ipairs(address) do
+        sg.engageSymbol(symbol)
+        sleep(0.5)
     end
-end
-end
 
-local function dialermenu()
-    while true do
-        term.clear()
-        print("\nStargate Dialer Menu:")
-        print("1. Dial Saved Address")
-        print("2. Dial Address")
-        print("3. Exit")
-
-        local choice = read()
-        if choice == "1" then
-            print("Enter the alias of the address you want to dial:")
-            local alias = read()
-            local address = addressManager.getAddress(alias)
-            dialermode(address)
-        elseif choice == "2" then
-            if config.isMilkyWay then
-                print("Enter Stargate address to dial (separated by dashes, e.g., 1-2-3):")
-                local input = read()
-                local address = {}
-                for num in string.gmatch(input, "%d+") do
-                    local symbol = tonumber(num)
-                    if symbol < 0 or symbol > 38 then
-                        error("Invalid symbol: " .. symbol)
-                    end
-                    table.insert(address, symbol)
-                end
-                while #address < 9 do
-                    table.insert(address, 0)
-                end
-                dialermode(address)
-            else
-                print("Sorry, this feature is only available for Milky Way Stargates.")
-            end
-        elseif choice == "3" then
-            print("Exiting...")
-            break
-        else
-            print("Invalid option, please try again.")
-        end
+    while not sg.isWormholeOpen() do
+        sleep(0.5)
     end
+
+    print("Stargate connected!")
 end
 
+-- Slow dialing for Stargate with optimized rotation direction
 local function slowDialStargate(address)
     local sg = peripheral.find("advanced_crystal_interface")
     print("Slow dialing Stargate...")
@@ -105,9 +66,72 @@ local function slowDialStargate(address)
     end
 end
 
+-- Prompt user for dialer mode (Fast or Slow)
+local function dialermode(address)
+    while true do
+        print("Choose dialing mode: (1) Fast, (2) Slow")
+        local mode = read()
+        if mode == "1" then
+            fastDialStargate(address)
+            break
+        elseif mode == "2" then
+            slowDialStargate(address)
+            break
+        else
+            print("Invalid option, please try again.")
+        end
+    end
+end
+
+-- Main menu for dialing
+local function dialermenu()
+    while true do
+        term.clear()
+        print("\nStargate Dialer Menu:")
+        print("1. Dial Saved Address")
+        print("2. Dial Address")
+        print("3. Exit")
+
+        local choice = read()
+        if choice == "1" then
+            print("Enter the alias of the address you want to dial:")
+            local alias = read()
+            local address = addressManager.getAddress(alias)
+            if address then
+                dialermode(address)
+            else
+                print("Alias not found.")
+            end
+        elseif choice == "2" then
+            if config.isMilkyWay then
+                print("Enter Stargate address to dial (separated by dashes, e.g., 1-2-3):")
+                local input = read()
+                local address = {}
+                for num in string.gmatch(input, "%d+") do
+                    local symbol = tonumber(num)
+                    if symbol < 0 or symbol > 38 then
+                        error("Invalid symbol: " .. symbol)
+                    end
+                    table.insert(address, symbol)
+                end
+                while #address < 9 do
+                    table.insert(address, 0)
+                end
+                dialermode(address)
+            else
+                print("Sorry, this feature is only available for Milky Way Stargates.")
+            end
+        elseif choice == "3" then
+            print("Exiting...")
+            break
+        else
+            print("Invalid option, please try again.")
+        end
+    end
+end
+
 return {
     fastDialStargate = fastDialStargate,
     slowDialStargate = slowDialStargate,
     dialermenu = dialermenu
 }
-
