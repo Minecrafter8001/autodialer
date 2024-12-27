b290f3d884d74fe33204fefe6ca053f2cd8c8ba0
minecrafter8001 <133237713+Minecrafter8001@users.noreply.github.com>
2024-12-27 01:15:53 +0000

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,21 @@
+-- config.lua
+
+local config = {
+    isMilkyWay = false -- Default to false
+}
+
+-- Get the Stargate peripheral
+local sg = peripheral.find("advanced_crystal_interface")
+
+-- Check if the Stargate peripheral is present
+if sg then
+    -- Get the Stargate type using getStargateType
+    local stargateType = sg.getStargateType()
+
+    -- Check if the Stargate is a Milky Way Stargate
+    if stargateType == "sgjourney:milky_way_stargate" then
+        config.isMilkyWay = true
+    end
+end
+
+return config
